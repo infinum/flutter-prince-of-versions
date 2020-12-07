2c1596363e5be32bd6bc1ccd173b62da6a514107
Dundo7 <maroje.marcelic@gmail.com>
2020-12-07 04:39:30 +0100

    Added some queen of versions methods

--- android/src/main/kotlin/com/example/flutter_prince_of_versions/FlutterPrinceOfVersionsPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_prince_of_versions/FlutterPrinceOfVersionsPlugin.kt
@@ -13,7 +13,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import com.example.flutter_prince_of_versions.
+
 
 class FlutterPrinceOfVersionsPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
@@ -25,7 +25,7 @@ class FlutterPrinceOfVersionsPlugin : FlutterPlugin, MethodCallHandler, Activity
         this.context = flutterPluginBinding.applicationContext
 
         channel = MethodChannel(
-                flutterPluginBinding.getFlutterEngine().getDartExecutor(),
+                flutterPluginBinding.getFlutterEngine().dartExecutor,
                 "flutter_prince_of_versions"
         )
 
@@ -35,10 +35,86 @@ class FlutterPrinceOfVersionsPlugin : FlutterPlugin, MethodCallHandler, Activity
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         when (call.method) {
             "check_for_updates" -> checkForUpdates((call.arguments as List<*>).first(), result)
-            "check_updates_from_store" -> checkForUpdatesFromPlayStore(result)
+            "check_updates_from_play_store" -> checkForUpdatesFromPlayStore(result)
         }
     }
-    private fun checkForUpdatesFromPlayStore(@NonNull flutterResult: Result) {
+    private fun checkForUpdatesFromPlayStore(@NonNull resultt: Result) {
+        val queenOfVersions = QueenOfVersions.Builder()
+                .build(this.activity as FragmentActivity)
+
+        val loader = NetworkLoader("http://pastebin.com/raw/QFGjJrLP")
+
+
+        println("nessto")
+        val callback = QueenOfVersions.Callback.Builder()
+                .withOnCanceled {
+                    println("on canceled")
+                }
+                .withOnMandatoryUpdateNotAvailable { requiredVersion, inAppUpdateInfo, metadata, updateInfo ->
+                    println("jesam li ovde?")
+                    println(requiredVersion)
+                    // for example show a message that update is required for application to work, but isn't available yet.
+
+                    // requiredVersion is version code of the update Prince of Versions claims is required
+                    // inAppUpdateInfo contains information about the update from Google Play
+                    // for metadata and updateInfo check Prince of Versions documentation
+                }
+                .withOnDownloaded { handler, info ->
+                    println("on downloaded")
+
+                }
+                .withOnDownloading { info, bytes, total ->
+                    println("on downloading")
+
+                }
+                .withOnError {
+                    resultt.success("woooo")
+                    println(it.localizedMessage)
+                    println(it.stackTrace.toString())
+                    println(it.message)
+                    println("on error")
+
+                }
+                .withOnInstalled {
+                    println("on installed")
+
+                }
+                .withOnInstalling {                     println("on installing")
+                }
+                .withOnUpdateAccepted { info, status, result ->
+                    println("on update accept")
+
+                }
+                .withOnUpdateDeclined { info, status, result ->
+                    println("on update decline")
+
+                }
+                .withOnNoUpdate { metadata, updateInfo ->
+                    println("on no update")
+
+                }
+                .withOnPending {
+                    println("on pending")
+
+                }
+                .build()
+
+        queenOfVersions.checkForUpdates(loader, callback);
+
+//        QueenOfVersions.checkForUpdates(
+//                this.activity as FragmentActivity,
+//                QueenOfVersions.Options.Builder()
+//                        .withOnInAppUpdateAvailable { currentStatus, _, _ ->
+//                            println("a tu buraz li ovde?")
+//                            println(currentStatus)
+//                            currentStatus
+//                        }
+//                        // use methods in builder to implement specific behavior
+//                        // there are equivalent methods as in QueenOfVersions builder
+//                        .build(),
+//                callback
+//        )
+
     }
 
     private fun checkForUpdates(url: Any?, @NonNull flutterResult: Result) {
@@ -49,7 +125,8 @@ class FlutterPrinceOfVersionsPlugin : FlutterPlugin, MethodCallHandler, Activity
                 flutterResult.success(result.toMap())
             }
             override fun onError(throwable: Throwable) {
-                flutterResult.error("1", "Invalid JSON", null)
+                println(throwable.toString())
+                flutterResult.error("1", throwable.message, null)
             }
         }
         updater.checkForUpdates(loader, callback)
@@ -92,7 +169,7 @@ fun UpdateInfo.toMap(): Map<String, Any> {
 
 fun UpdateStatus.toMap(): String {
     return when(this) {
-        UpdateStatus.NEW_UPDATE_AVAILABLE -> Constants.UPDATE-AVAILABLE
+        UpdateStatus.NEW_UPDATE_AVAILABLE -> Constants.UPDATE_AVAILABLE
         UpdateStatus.NO_UPDATE_AVAILABLE -> "no-update"
         UpdateStatus.REQUIRED_UPDATE_NEEDED -> "required-update"
     }
