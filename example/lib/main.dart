2215555fff1a26d559437f1b77f9f8c4e8563f42
Maroje Marcelić <37837673+Dundo7@users.noreply.github.com>
2020-12-11 09:21:31 +0100

    Update example/lib/main.dart
    Co-authored-by: Vlaho Poluta <v.poluta@hotmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -63,7 +63,7 @@ class _MyAppState extends State<MyApp> {
           CupertinoButton.filled(
               child: Text('Play Store test'),
               onPressed: () async {
-                final Callback c = MyCallback(context);
+                final callback = MyCallback(context);
                 await FlutterPrinceOfVersions.checkForUpdatesFromGooglePlay("http://pastebin.com/raw/QFGjJrLP", c);
               }),
         ],
